4c4e1b51b66a17a81a7071d4fd64f843274ed281
iamArvy <okeseyi5@gmail.com>
2024-08-28 00:46:03 +0100

    started footer

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,5 +1,26 @@
+<script setup>
+import NavList from './FooterNavList.vue'
+const items = [
+  {name: "about", url: "/"},
+  {name: "about", url: "/"},
+  {name: "about", url: "/"},
+  {name: "about", url: "/"},
+]
+</script>
+
 <template>
   <section>
+    <div class="top">
+      <div class="footer-about">
+        <div class="logo"><img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" /></div>
+        <p>We have Shoes that suits your style and which you're proud to wear, From women to men.</p>
+        <div class="socials"></div>
+      </div>
+      <NavList :items="items">Company</NavList>
+      <NavList :items="items">Help</NavList>
+      <NavList :items="items">FAQ</NavList>
+      <NavList :items="items">RESOURCES</NavList>
+    </div>
     <h1>Hi</h1>
   </section>
 </template>
---
+++ src/components/FooterNavList.vue
@@ -0,0 +1,22 @@
+<script setup>
+// import { defineComponent } from '@vue/composition-api'
+defineProps({
+    items: Array
+})
+</script>
+
+<template>
+    <div class="footer-nav-list">
+        <h2><slot /></h2>
+        <ul>
+          <li v-for="item in items" :key="item">
+            <router-link :to="item.url">{{ item.name }}</router-link>
+          </li>
+        </ul>
+      </div>
+</template>
+<style scoped>
+h2{
+    text-transform: uppercase;
+}
+</style>
